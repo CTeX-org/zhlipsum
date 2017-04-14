15642aeed26cae19816f8a60d0c87de5f3ad6919
Stone-Zeng <pssysrq@163.com>
2017-04-14 13:59:13 +0800

    update: batch files

--- source/install.bat
+++ source/install.bat
@@ -4,7 +4,9 @@ xetex zhlipsum.ins
 
 copy /Y "zhlipsum.sty"       "..\zhlipsum.sty"
 copy /Y "zhlipsum-zh-cn.def" "..\zhlipsum-zh-cn.def"
+copy /Y "zhlipsum.sty"       "..\test\zhlipsum.sty"
+copy /Y "zhlipsum-zh-cn.def" "..\test\zhlipsum-zh-cn.def"
 
-delete-aux.bat
 del *.sty
 del *.def
+delete-aux.bat
