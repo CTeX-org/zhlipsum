00df94db42974dbbe6ed268c03a77b002df4d528
Stone-Zeng <pssysrq@163.com>
2017-09-16 20:26:46 +0800

    add `encoding` option; refactoring; update test files; add test for latex and uplatex

--- support/texlive.sh
+++ support/texlive.sh
@@ -22,79 +22,72 @@ if ! command -v texlua > /dev/null; then
   cd ..
 fi
 
-# Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
-
-# Required to build plain and LaTeX formats:
-# TeX90 plain for unpacking, pdfLaTeX, LuaLaTeX and XeTeX for tests
-tlmgr install cm etex knuth-lib latex-bin tex tex-ini-files unicode-data \
-  xetex
-  
-# Additional requirements for (u)pLaTeX, done with no dependencies to
-# avoid large font payloads
-tlmgr install --no-depends babel ptex uptex ptex-base uptex-base ptex-fonts \
-  uptex-fonts platex uplatex
-
-# Assuming a 'basic' font set up, metafont is required to avoid
-# warnings with some packages and errors with others
-tlmgr install metafont mfware
-
-
-# Set up graphics: nowadays split over a few places and requiring
-# HO's bundle
-tlmgr install graphics graphics-cfg graphics-def oberdiek
-
-# Contrib packages for testing
-#
-# The packages themselves are done with --no-depends to avoid
-# picking up l3kernel, etc.
-#
-tlmgr install ifluatex lm lualibs luaotfload
-
-# Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# Dependencies other than the core l3build set up, metafont, fontspec and the
-# 'graphics stack' (itself needed by fontspec) are listed below
+# TeX base
 tlmgr install     \
+  tex             \
+  etex            \
+  knuth-lib       \
+  latex-bin       \
+  tex-ini-files   \
+  cm
+
+# Fonts
+tlmgr install     \
+  adobemapping    \
+  amsfonts        \
+  fandol          \
+  lm              \
+  lm-math         \
+  metafont        \
+  mfware          \
+  psnfss
+
+# Other
+tlmgr install     \
+  amsmath         \
+  caption         \
+  ctablestack     \
+  ctex            \
+  currfile        \
+  environ         \
+  etoolbox        \
+  fancyhdr        \
+  filehook        \
+  graphics        \
+  graphics-cfg    \
+  graphics-def    \
+  ifluatex        \
+  ifpdf           \
+  ifxetex         \
+  kantlipsum      \
   l3kernel        \
   l3experimental  \
   l3packages      \
   l3build         \
-  adobemapping    \
-  amsfonts        \
-  amsmath         \
-  chemformula     \
-  chemgreek       \
-  cjk             \
-  cjkpunct        \
-  ctablestack     \
-  ctex            \
-  ec              \
-  environ         \
-  etoolbox        \
-  fandol          \
-  filehook        \
-  fontspec        \
-  ifxetex         \
-  kantlipsum      \
-  lm-math         \
   lualatex-math   \
+  lualibs         \
+  luaotfload      \
+  luatex          \
+  luatex85        \
   luatexbase      \
   luatexja        \
-  mhchem          \
   ms              \
-  pgf             \
-  siunitx         \
+  oberdiek        \
+  preview         \
   tools           \
   trimspaces      \
   ucharcat        \
   ulem            \
-  unicode-math    \
-  units           \
+  unicode-data    \
+  uplatex         \
+  varwidth        \
   xcolor          \
   xecjk           \
+  xetex           \
   xkeyval         \
   xunicode        \
-  zhmetrics       \
+  zh­metrics       \
+  zh­metrics-up­tex \
   zhnumber
 
 # Keep no backups (not required, simply makes cache bigger)
---
+++ support/zhconvert.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# This script is used for changing encoding from UTF-8 to GBK.
+
+jobname=zhlipsum-zh-cn
+
+mv "$jobname-gbk.def" "$jobname-gbk.old.def"
+
+iconv --from-code=utf8 --to-code=gbk  \
+  --output="$jobname-gbk.def"         \
+  "$jobname-gbk.old.def"
+
+rm "$jobname-gbk.old.def"
