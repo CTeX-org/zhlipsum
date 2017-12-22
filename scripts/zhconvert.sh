de0ca9a8aaffa09e9288d87c31e71949506a60fd
Stone-Zeng <pssysrq@163.com>
2017-12-22 14:30:58 +0800

    add BIG5 encoding [ci skip]

--- scripts/zhconvert.sh
+++ scripts/zhconvert.sh
@@ -2,15 +2,10 @@
 
 # This script is used for changing encoding from UTF-8 to GBK.
 
-mv "zhlipsum-simp-gbk.def" "zhlipsum-simp-gbk.old.def"
-mv "zhlipsum-trad-gbk.def" "zhlipsum-trad-gbk.old.def"
+mv zhlipsum-gbk.def  zhlipsum-gbk.tmp.def
+mv zhlipsum-big5.def zhlipsum-big5.tmp.def
 
-iconv --from-code=utf8 --to-code=gbk  \
-  --output="zhlipsum-simp-gbk.def"         \
-  "zhlipsum-simp-gbk.old.def"
-iconv --from-code=utf8 --to-code=gbk  \
-  --output="zhlipsum-trad-gbk.def"         \
-  "zhlipsum-trad-gbk.old.def"
+iconv --from-code=utf8 --to-code=gbk  --output=zhlipsum-gbk.def  zhlipsum-gbk.tmp.def
+iconv --from-code=utf8 --to-code=big5 --output=zhlipsum-big5.def zhlipsum-big5.tmp.def
 
-rm "zhlipsum-simp-gbk.old.def"
-rm "zhlipsum-trad-gbk.old.def"
+rm *.tmp.def
