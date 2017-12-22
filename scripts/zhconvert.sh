9ba5c614bbbccca10f17f1b456c9b00af6ff6f5b
Stone-Zeng <pssysrq@163.com>
2017-12-22 12:29:27 +0800

    add traditional Chinese text

--- scripts/zhconvert.sh
+++ scripts/zhconvert.sh
@@ -2,12 +2,15 @@
 
 # This script is used for changing encoding from UTF-8 to GBK.
 
-jobname=zhlipsum-simp
-
-mv "$jobname-gbk.def" "$jobname-gbk.old.def"
+mv "zhlipsum-simp-gbk.def" "zhlipsum-simp-gbk.old.def"
+mv "zhlipsum-trad-gbk.def" "zhlipsum-trad-gbk.old.def"
 
 iconv --from-code=utf8 --to-code=gbk  \
-  --output="$jobname-gbk.def"         \
-  "$jobname-gbk.old.def"
+  --output="zhlipsum-simp-gbk.def"         \
+  "zhlipsum-simp-gbk.old.def"
+iconv --from-code=utf8 --to-code=gbk  \
+  --output="zhlipsum-trad-gbk.def"         \
+  "zhlipsum-trad-gbk.old.def"
 
-rm "$jobname-gbk.old.def"
+rm "zhlipsum-simp-gbk.old.def"
+rm "zhlipsum-trad-gbk.old.def"
