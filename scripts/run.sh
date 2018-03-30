a70daa82f3dda3ca33c72a7a7c3e192c484b40c1
Stone-Zeng <xdzeng96@gmail.com>
2018-03-30 17:32:18 +0800

    test: add encoding tests
    Rename test files.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,8 +3,35 @@
 export SAVE="texlua build.lua save"
 export CHECK="texlua build.lua check --halt-on-error"
 
-export TESTFILES_A="01-internal 02-api 03-user 04-compilation"
-export TESTFILES_B="05-gbk 06-big5"
+export TESTFILES_A=\
+"
+internal
+api
+user
+compilation-utf8
+encodings01
+encodings02
+encodings03
+encodings04
+encodings05
+encodings06
+encodings07
+encodings08
+"
+
+export TESTFILES_B=\
+"
+compilation-gbk
+compilation-big5
+encodings01
+encodings02
+encodings03
+encodings04
+encodings05
+encodings06
+encodings07
+encodings08
+"
 
 if [ "$1" = "save" ]; then
   $SAVE --engine xetex  $TESTFILES_A
