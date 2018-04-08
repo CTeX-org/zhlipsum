84d0fc131546b55bc84118a928f0655d493c68b3
Zeng Xiangdong <xdzeng96@gmail.com>
2018-04-08 20:22:13 +0800

    build: update release.sh [ci skip]

--- scripts/release.sh
+++ scripts/release.sh
@@ -32,7 +32,7 @@ cp $WORKING_DIR/source/*.dtx $TEMP_DIR
 cp $WORKING_DIR/source/*.pdf $TEMP_DIR
 
 cd $TEMP_DIR
-xetex $JOB_NAME.dtx >/dev/null
+xetex $JOB_NAME.dtx > /dev/null
 texlua $DOC_EN_SCRIPT $JOB_NAME.dtx $JOB_NAME-en.tex
 $ZHCONVERT_SCRIPT
 
@@ -54,10 +54,10 @@ zip -q -r -9 $JOB_NAME.tds.zip .
 cp $TEMP_DIR/*.dtx $CTAN_DIR
 cp $TEMP_DIR/*.md  $CTAN_DIR
 cp $TEMP_DIR/*.pdf $CTAN_DIR
-cp $TDS_DIR/*.zip  $CTAN_DIR
 
-rm -r $TDS_DIR
 rm $TEMP_DIR/*.*
+cp $TDS_DIR/*.zip $TEMP_DIR
+rm -r $TDS_DIR
 
 # Make CTAN zip
 cd $TEMP_DIR
@@ -65,6 +65,6 @@ zip -q -r -9 $JOB_NAME.zip .
 
 cd $WORKING_DIR
 cp -f $TEMP_DIR/$JOB_NAME.zip     .
-cp -f $CTAN_DIR/$JOB_NAME.tds.zip .
+cp -f $TEMP_DIR/$JOB_NAME.tds.zip .
 
 rm -r $TEMP_DIR
