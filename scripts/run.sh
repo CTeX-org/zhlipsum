93b42b9d7811a631b8a31a0eabfe07272031c65d
Xiangdong Zeng <xdzeng96@gmail.com>
2018-07-21 11:46:37 +0800

    ci: debug-10
    Minimal test.

--- scripts/run.sh
+++ scripts/run.sh
@@ -52,10 +52,11 @@ compilation-big5
 
 if [ "$2" = "--docker" ]; then
   $DOCKER ls -al
-  $DOCKER /bin/bash -c "pdflatex hello.tex; cat /miktex/.miktex/texmfs/data/miktex/log/pdflatex.log"
-  $DOCKER /bin/bash -c "xelatex hello.tex; cat /miktex/.miktex/texmfs/data/miktex/log/xelatex.log"
+  #$DOCKER /bin/bash -c "pdflatex hello.tex; cat /miktex/.miktex/texmfs/data/miktex/log/pdflatex.log"
+  #$DOCKER /bin/bash -c "xelatex hello.tex; cat /miktex/.miktex/texmfs/data/miktex/log/xelatex.log"
+  $DOCKER pdflatex hello.tex
   $DOCKER pdflatex hello-zh.tex
-  $DOCKER xelatex hello-zh.tex
+  #$DOCKER xelatex hello-zh.tex
   #if [ "$1" = "check-utf8" ]; then
   #  $DOCKER $CHECK $TESTFILES_A
   #elif [ "$1" = "check-gbk-big5" ]; then
