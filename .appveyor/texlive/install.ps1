d9b99b541569f13ce37c0d1a778db5bebbb18e5b
Xiangdong Zeng <xdzeng96@gmail.com>
2018-09-08 15:53:47 +0800

    ci: debug-5

--- .appveyor/texlive/install.ps1
+++ .appveyor/texlive/install.ps1
@@ -19,3 +19,81 @@ Get-ChildItem "C:\texlive\bin\win32"
 Get-ChildItem "C:\texlive\texmf-dist"
 
 Get-Content "C:\texlive\install-tl.log"
+
+# Change default package repository
+tlmgr option repository https://mirrors.rit.edu/CTAN/systems/texlive/tlnet
+
+# Packages
+tlmgr install     \
+  adobemapping    \
+  amsfonts        \
+  amsmath         \
+  caption         \
+  cjk             \
+  cjkpunct        \
+  cjkutils        \
+  cm              \
+  ctablestack     \
+  ctex            \
+  currfile        \
+  dvipdfmx        \
+  environ         \
+  etex            \
+  etoolbox        \
+  euenc           \
+  fancyhdr        \
+  fandol          \
+  filehook        \
+  fontspec        \
+  graphics        \
+  graphics-cfg    \
+  graphics-def    \
+  ifluatex        \
+  ifxetex         \
+  kantlipsum      \
+  knuth-lib       \
+  l3build         \
+  l3experimental  \
+  l3kernel        \
+  l3packages      \
+  latex-bin       \
+  lm              \
+  lm-math         \
+  lualatex-math   \
+  lualibs         \
+  luaotfload      \
+  luatex          \
+  luatex85        \
+  luatexbase      \
+  luatexja        \
+  metafont        \
+  mfware          \
+  ms              \
+  oberdiek        \
+  platex-tools    \
+  preview         \
+  psnfss          \
+  tex             \
+  tex-ini-files   \
+  tools           \
+  trimspaces      \
+  ucharcat        \
+  ulem            \
+  unicode-data    \
+  uplatex         \
+  varwidth        \
+  xcjk2uni        \
+  xcolor          \
+  xecjk           \
+  xetex           \
+  xkeyval         \
+  xunicode        \
+  zhmetrics       \
+  zhmetrics-uptex \
+  zhnumber
+
+# Keep no backups (not required, simply makes cache bigger)
+tlmgr option -- autobackup 0
+
+# Update the TL install but add nothing new
+tlmgr update --self --all --no-auto-install
