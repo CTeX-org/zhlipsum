b9482b32163a22fc860006241ba50068169bd549
Xiangdong Zeng <xdzeng96@gmail.com>
2018-09-08 14:45:52 +0800

    script: add back-tick for ps1 line continuation

--- .appveyor/texlive/install.ps1
+++ .appveyor/texlive/install.ps1
@@ -1,11 +1,11 @@
 Get-ChildItem
-Invoke-WebRequest -Uri "https://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl.zip"
+Invoke-WebRequest -Uri "https://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl.zip" `
                   -OutFile "install-tl.zip"
 Expand-Archive "install-tl.zip" -DestinationPath .
 Set-Location "install-tl-20*"
 
-.\install-tl-advanced.bat -no-gui
-                          -profile     ../.appveyor/texlive/texlive.profile
+.\install-tl-advanced.bat -no-gui `
+                          -profile     ../.appveyor/texlive/texlive.profile `
                           -repository  https://mirrors.rit.edu/CTAN/systems/texlive/tlnet
 
 Write-Output $env:PATH
