c9749fa45ac6cc6ecd745c5c63c25476d5008176
Xiangdong Zeng <xdzeng96@gmail.com>
2018-09-08 14:39:27 +0800

    ci: debug-3
    Use powershell script.

---
+++ .appveyor/texlive/install.ps1
@@ -0,0 +1,16 @@
+Get-ChildItem
+Invoke-WebRequest -Uri "https://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl.zip"
+                  -OutFile "install-tl.zip"
+Expand-Archive "install-tl.zip" -DestinationPath .
+Set-Location "install-tl-20*"
+
+.\install-tl-advanced.bat -no-gui
+                          -profile     ../.appveyor/texlive/texlive.profile
+                          -repository  https://mirrors.rit.edu/CTAN/systems/texlive/tlnet
+
+Write-Output $env:PATH
+Write-Output $env:PROGRAMFILES
+Write-Output $env:USERPROFILE
+
+Get-ChildItem $env:PROGRAMFILES
+Get-ChildItem $env:USERPROFILE
