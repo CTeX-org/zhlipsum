daade3570c87b3811b029b20b79fa7832cb59cda
Xiangdong Zeng <xdzeng96@gmail.com>
2018-09-08 16:27:02 +0800

    ci: debug-8
    Try l3build.

--- .appveyor/texlive/install.ps1
+++ .appveyor/texlive/install.ps1
@@ -1,24 +1,19 @@
-Get-ChildItem
-Invoke-WebRequest -Uri "https://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl.zip" `
-                  -OutFile "install-tl.zip"
-Expand-Archive "install-tl.zip" -DestinationPath .
-Set-Location "install-tl-20*"
-
-.\install-tl-advanced.bat -no-gui `
-                          -profile     ../.appveyor/texlive/texlive.profile `
-                          -repository  https://mirrors.rit.edu/CTAN/systems/texlive/tlnet
-
-Write-Output $env:PATH
 $env:PATH += ";C:\texlive\bin\win32"
-Write-Output $env:PATH
 
-# DEBUG
-Get-ChildItem "C:\texlive"
-Get-ChildItem "C:\texlive\bin"
-Get-ChildItem "C:\texlive\bin\win32"
-Get-ChildItem "C:\texlive\texmf-dist"
+# See if there is a cached version of TL available
+if (-Not (Get-Command texlua -errorAction SilentlyContinue))
+{
+  Invoke-WebRequest -Uri "https://mirrors.rit.edu/CTAN/systems/texlive/tlnet/install-tl.zip" `
+                    -OutFile "install-tl.zip"
+  Expand-Archive "install-tl.zip" -DestinationPath .
+  Set-Location "install-tl-20*"
 
-Get-Content "C:\texlive\install-tl.log"
+  # Install a minimal system
+  .\install-tl-advanced.bat -no-gui `
+                            -profile     ../.appveyor/texlive/texlive.profile `
+                            -repository  https://mirrors.rit.edu/CTAN/systems/texlive/tlnet
+  Set-Location ..
+}
 
 # Change default package repository
 tlmgr option repository https://mirrors.rit.edu/CTAN/systems/texlive/tlnet
